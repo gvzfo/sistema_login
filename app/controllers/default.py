7e9d289b585cef4beb5d8839fb2cfd4e9c2bbba0
gvzfo <gvzfo@yahoo.com.br>
2018-03-17 11:09:17 -0300

    integracao sqlalchemy e bottle

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,24 +1,26 @@
 from app import app
-from bottle import request, template
-from bottle import static_file
+# from app import create_session
+from app.models.tables import User
+from bottle import request, template, static_file
+
 
 
 # static routes
 @app.get('/<filename:re:.*\.css>')
 def stylesheets(filename):
-    return static_file(filename, root='static/css')
+    return static_file(filename, root='app/static/css')
 
 @app.get('/<filename:re:.*\.js>')
 def javascript(filename):
-    return static_file(filename, root='static/js')
+    return static_file(filename, root='app/static/js')
 
 @app.get('/<filename:re:.*\.(jpg|png|gif|ico)>')
 def images(filename):
-    return static_file(filename, root='static/img')
+    return static_file(filename, root='app/static/img')
 
 @app.get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
 def fonts(filename):
-    return static_file(filename, root='static/fonts')
+    return static_file(filename, root='app/static/fonts')
 
 
 # @route('/')
@@ -45,10 +47,17 @@ def cadastro():
 
 
 @app.route('/cadastro', method='POST')
-def acao_cadastro():
+def acao_cadastro(db):
+
     username = request.forms.get('username')
     password = request.forms.get('password')
-    insert_user(username, password)
+    new_user = User(username, password)
+    db.add(new_user)
+    # session = create_session()
+    # new_user = User(username, password)
+    # session.add(new_user)
+    # session.commit()
+    # insert_user(username, password)
     return template('verificacao_cadastro', nome=username)
 
 
@@ -70,4 +79,3 @@ def acao_login():
 @app.error(404)
 def error404(error):
     return template('pagina404')
-
